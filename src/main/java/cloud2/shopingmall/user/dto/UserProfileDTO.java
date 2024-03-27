65654a6d95fa987753c87fee09c647e119970b5c
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-03-27 23:28:04 +0900

    DTO Vailid 적용

--- src/main/java/cloud2/shopingmall/user/dto/UserProfileDTO.java
+++ src/main/java/cloud2/shopingmall/user/dto/UserProfileDTO.java
@@ -1,5 +1,7 @@
 package cloud2.shopingmall.user.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +11,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserProfileDTO {
 
+    @NotNull(message = "이름을 입력해 주세요.")
+    @Pattern(regexp = "^[가-힣]*$", message = "한글만 입력 가능합니다.")
     private String userName;
+
+    @NotNull(message = "이메일을 입력해 주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "올바른 이메일 주소 형식이 아닙니다.")
     private String userEmail;
+
+    @NotNull(message = "핸드폰 번호를 입력해 주세요.")
+    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "올바른 핸드폰 번호 형식이 아닙니다.")
     private String userPhoneNumber;
+
+    @NotNull(message = "주소를 입력해 주세요.")
     private String userAddress;
+
+    @NotNull(message = "성별을 선택해 주세요.")
     private Gender gender;
+
+    @NotNull(message = "생년월일을 입력해 주세요.")
     private String birthDate;
 
 
--- src/main/java/cloud2/shopingmall/user/service/JoinService.java
+++ src/main/java/cloud2/shopingmall/user/service/JoinService.java
@@ -2,7 +2,12 @@ package cloud2.shopingmall.user.service;
 
 
 import cloud2.shopingmall.user.dto.UserDTO;
+import cloud2.shopingmall.user.dto.UserProfileDTO;
 import cloud2.shopingmall.user.entity.User;
+import cloud2.shopingmall.user.entity.UserProfile;
+import cloud2.shopingmall.user.mapper.UserMainMapper.UserProfileMapper;
+import cloud2.shopingmall.user.mapper.UserMainMapper.UserMapper;
+import cloud2.shopingmall.user.repository.UserProfileRepository;
 import cloud2.shopingmall.user.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,33 +16,49 @@ import org.springframework.stereotype.Service;
 public class JoinService {
 
     private final UserRepository userRepository;
+    private final UserProfileRepository userProfileRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserMapper userMapper;
+    private final UserProfileMapper userProfileMapper;
 
 
-    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public JoinService(
+            UserRepository userRepository,
+            UserProfileRepository userProfileRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder,
+            UserMapper userMapper,
+            UserProfileMapper userProfileMapper) {
 
         this.userRepository = userRepository;
+        this.userProfileRepository = userProfileRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userMapper = userMapper;
+        this.userProfileMapper = userProfileMapper;
     }
 
-    public void joinProcess(UserDTO userDTO) {
+    public void joinProcess(UserDTO userDTO, UserProfileDTO userProfileDTO) {
 
         String username = userDTO.getUsername();
         String password = userDTO.getPassword();
 
+        // repository에 유저 정보가 존재하는지 체크 존재하는 경우 true 없으면 false
         Boolean isExist = userRepository.existsByUsername(username);
 
         if (isExist) {
-
+            // 현재 존재하는 경우 바로 리턴
             return;
         }
 
-        User data = new User();
+        // 없을 경우 다음 로직 실행
+        // User Entity 부분
+        User user = userMapper.toEntity(userDTO);
+        user.setPassword(bCryptPasswordEncoder.encode(password)); // 비밀번호를 암호화하여 저장
+        user.setUserRole(User.UserRole.ADMIN); // Role 부여
 
-        data.setUsername(username);
-        data.setPassword(bCryptPasswordEncoder.encode(password)); // 비밀번호를 암호화하여 저장
-        data.setUserRole(User.UserRole.ADMIN); // Role 부여
+        // UserProfile Entity 부분
+        UserProfile userProfile = userProfileMapper.toEntity(userProfileDTO);
 
-        userRepository.save(data);
+
+        userRepository.save(user);
     }
 }
