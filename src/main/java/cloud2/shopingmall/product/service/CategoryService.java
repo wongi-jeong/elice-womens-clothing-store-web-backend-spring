9fcf26764d48a9c2b4528b63d5f0ff2bb26abf8e
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-03-28 13:37:28 +0900

    feat: CategoryService 클래스에 생성, 추가, 삭제 메서드 구현

--- src/main/java/cloud2/shopingmall/product/service/CategoryService.java
+++ src/main/java/cloud2/shopingmall/product/service/CategoryService.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private Category foundCategory;
 
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
@@ -21,9 +23,35 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
-    public Category findCategory(Long categoryId) {
-        return categoryRepository.findById(categoryId)
+    public Category findCategory(Long id) {
+        return categoryRepository.findById(id)
                 .orElseThrow(NoSuchElementException::new);
     }
 
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Category category) {
+        foundCategory = categoryRepository.findById(category.getId())
+                .orElseThrow(NoSuchElementException::new);
+
+        Optional.ofNullable(category.getCategoryName())
+                .ifPresent(categoryName -> foundCategory = foundCategory.toBuilder()
+                        .categoryName(categoryName)
+                        .build());
+
+        foundCategory = foundCategory.toBuilder()
+                .categoryRank(category.getCategoryRank())
+                .build();
+
+        return categoryRepository.save(foundCategory);
+    }
+
+    public void deleteCategory(Long id) {
+        foundCategory = categoryRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new);
+
+        categoryRepository.delete(foundCategory);
+    }
 }
