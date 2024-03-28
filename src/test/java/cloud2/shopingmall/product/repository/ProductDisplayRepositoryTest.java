8fdb0bed2f6deb6f5dc3115bfa0093313566939c
BugeonJo <bugeonjo91@gmail.com>
2024-03-28 14:35:34 +0900

    FIX: productDisplayRepositoryTest 수정

--- src/test/java/cloud2/shopingmall/product/repository/ProductDisplayRepositoryTest.java
+++ src/test/java/cloud2/shopingmall/product/repository/ProductDisplayRepositoryTest.java
@@ -3,6 +3,7 @@ package cloud2.shopingmall.product.repository;
 
 import cloud2.shopingmall.product.entity.Product;
 import cloud2.shopingmall.product.entity.ProductDisplay;
+import cloud2.shopingmall.product.entity.ProductDisplayImage;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ProductDisplayRepositoryTest {
 
     private final ProductDisplayRepository productDisplayRepository;
+    private final ProductDisplayImageRepository productDisplayImageRepository;
     private final ProductRepository productRepository;
     @Autowired
-    public ProductDisplayRepositoryTest(ProductDisplayRepository productDisplayRepository, ProductRepository productRepository) {
+    public ProductDisplayRepositoryTest(ProductDisplayRepository productDisplayRepository, ProductRepository productRepository, ProductDisplayImageRepository productDisplayImageRepository) {
 
         this.productDisplayRepository = productDisplayRepository;
         this.productRepository = productRepository;
+        this.productDisplayImageRepository = productDisplayImageRepository;
     }
 
 
@@ -33,20 +36,33 @@ public class ProductDisplayRepositoryTest {
         ProductDisplay pd = new ProductDisplay("elice", "description", "url");
         Product product = new Product("elice", Product.Size.ONE, Product.Color.ONE, 100, Product.ProductStatus.ONE);
         Product product2 = new Product("elice2", Product.Size.TWO, Product.Color.TWO, 200, Product.ProductStatus.TWO);
+
+        ProductDisplayImage productDisplayImage = new ProductDisplayImage("eliceImage", "description");
+        ProductDisplayImage productDisplayImage2 = new ProductDisplayImage("eliceImage2", "description");
+
+
         pd.getProducts().add(product);
         pd.getProducts().add(product2);
+        pd.getProductDisplayImages().add(productDisplayImage);
+        pd.getProductDisplayImages().add(productDisplayImage2);
         product.setProductDisplay(pd);
         product2.setProductDisplay(pd);
+        productDisplayImage.setProductDisplay(pd);
+        productDisplayImage2.setProductDisplay(pd);
 
         ProductDisplay pd2 = productDisplayRepository.save(pd);
         productRepository.save(product);
         productRepository.save(product2);
+        productDisplayImageRepository.save(productDisplayImage);
+        productDisplayImageRepository.save(productDisplayImage2);
 
         ProductDisplay pd3 = productDisplayRepository.findProductDisplayWithOptions(pd2.getId());
 
         assertEquals(pd3.getProducts().get(0).getColor(), Product.Color.ONE);
         assertEquals(pd3.getProducts().get(1).getColor(), Product.Color.TWO);
 
+        assertEquals(pd3.getProductDisplayImages().get(0).getImageUrl(), "eliceImage");
+        assertEquals(pd3.getProductDisplayImages().get(1).getImageUrl(), "eliceImage2");
 
 
 
