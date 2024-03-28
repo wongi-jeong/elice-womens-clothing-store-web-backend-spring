e48bd737af2df039ed24563dc22c14a34e0ad1ff
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-03-28 21:00:41 +0900

    feat: CategoryController 클래스 생성 및 REST API 구현

---
+++ src/main/java/cloud2/shopingmall/product/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package cloud2.shopingmall.product.controller;
+
+import cloud2.shopingmall.product.dto.CategoryDTO;
+import cloud2.shopingmall.product.entity.Category;
+import cloud2.shopingmall.product.mapper.ProductMainMapper;
+import cloud2.shopingmall.product.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService, ProductMainMapper.CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.findCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable Long id) {
+        return categoryService.findCategory(id);
+    }
+
+    @PostMapping("/create")
+    public Category createCategory(@RequestBody CategoryDTO categoryDTO) {
+        return categoryService.saveCategory(categoryDTO);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+        categoryDTO.setId(id);
+        return categoryService.updateCategory(categoryDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+
+}
