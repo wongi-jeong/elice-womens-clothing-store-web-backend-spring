0a154a06f103a747c0da4d1781d341675d907a49
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-04-02 15:37:14 +0900

    회원정보 변경기능 구현 완료

--- src/main/java/cloud2/shopingmall/order/controller/OrderController.java
+++ src/main/java/cloud2/shopingmall/order/controller/OrderController.java
@@ -53,16 +53,16 @@ public class OrderController {
         }
         return ResponseEntity.ok(orderDTO);
     }
-//    @PostMapping("/create")
-//    public ResponseEntity<OrderDTO> createOrderByCart(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-//                                                @RequestBody @Validated OrderCreateRequest.OrderByCart orderCreateRequest){
-//        //장바구니 주문
-////json으로 여러 객체 받아와야할때 복합 객체를 사용하게 되면 언제 검증을 하게 되는지 그리고 검증에 관한 로직은 어디서 구현하면 좋을 지
-//        OrderDTO orderDTO = orderManagementService.createOrderForCart(customUserDetails.getUsername());
-//        orderManagementService.createOrderProduct(orderCreateRequest.getOrderProductDTOS(),orderDTO.getId());
-//        return ResponseEntity.ok(orderDTO);
-//
-//    }
+    @PostMapping("/create")
+    public ResponseEntity<OrderDTO> createOrderByCart(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                @RequestBody @Validated OrderCreateRequest.OrderByCart orderCreateRequest){
+        //장바구니 주문
+//json으로 여러 객체 받아와야할때 복합 객체를 사용하게 되면 언제 검증을 하게 되는지 그리고 검증에 관한 로직은 어디서 구현하면 좋을 지
+        OrderDTO orderDTO = orderManagementService.createOrderForCart(customUserDetails.getUsername());
+        orderManagementService.createOrderProduct(orderCreateRequest.getOrderProductDTOS(),orderDTO.getId());
+        return ResponseEntity.ok(orderDTO);
+
+    }
     @PostMapping("/create")
     public ResponseEntity<OrderDTO> createOrderByProduct(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                       @RequestBody @Validated OrderCreateRequest.OrderByProduct orderByProduct){
