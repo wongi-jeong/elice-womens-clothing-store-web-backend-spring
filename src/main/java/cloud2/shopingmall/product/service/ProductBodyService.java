5cd312f43e38102ffafe2804dc2137b5f6a29140
BugeonJo <bugeonjo91@gmail.com>
2024-04-02 18:06:11 +0900

    FEAT: ProductBody controller 구현, 서비스 단 일부 수정(DTO 메서드 구현)

--- src/main/java/cloud2/shopingmall/product/service/ProductBodyService.java
+++ src/main/java/cloud2/shopingmall/product/service/ProductBodyService.java
@@ -1,8 +1,10 @@
 package cloud2.shopingmall.product.service;
 
+import cloud2.shopingmall.product.dto.ProductBodyDTO;
 import cloud2.shopingmall.product.entity.Product;
 import cloud2.shopingmall.product.entity.ProductBody;
 import cloud2.shopingmall.product.entity.ProductDetails;
+import cloud2.shopingmall.product.mapper.ProductMainMapper;
 import cloud2.shopingmall.product.repository.ProductBodyRepository;
 import cloud2.shopingmall.product.repository.ProductDetailsRepository;
 import cloud2.shopingmall.product.repository.ProductRepository;
@@ -19,10 +21,14 @@ public class ProductBodyService {
     private final ProductBodyRepository productBodyRepository;
     private final ProductRepository productRepository;
 
+    private final ProductMainMapper.ProductBodyMapper productBodyMapper;
+
+
     @Autowired
-    public ProductBodyService(ProductBodyRepository productBodyRepository, ProductRepository productRepository) {
+    public ProductBodyService(ProductBodyRepository productBodyRepository, ProductRepository productRepository, ProductMainMapper.ProductBodyMapper productBodyMapper) {
         this.productBodyRepository = productBodyRepository;
         this.productRepository = productRepository;
+        this.productBodyMapper = productBodyMapper;
     }
 
     public ProductBody getProductBody(Long id) {
@@ -32,7 +38,9 @@ public class ProductBodyService {
     public List<ProductBody> getProductBodiesByProductId(Long productId) {
         return productBodyRepository.findByProduct_Id(productId);
     }
-
+    public List<ProductBodyDTO> getProductBodiesDTOByProductId(Long productId) {
+        return productBodyMapper.toDto(productBodyRepository.findByProduct_Id(productId));
+    }
 
     public List<ProductBody> saveProductBodies(List<ProductBody> productBodies, Long productId) {
         Product product = productRepository.getReferenceById(productId);
@@ -44,6 +52,20 @@ public class ProductBodyService {
 
         return productBodyRepository.saveAll(productBodies);
     }
+
+    public List<ProductBodyDTO> saveProductBodiesDTO(List<ProductBodyDTO> productBodiesDTO, Long productId) {
+        Product product = productRepository.getReferenceById(productId);
+        List<ProductBody> productBodies = productBodyMapper.toEntity(productBodiesDTO);
+
+        for(int i = 0; i < productBodies.size(); i++){
+            productBodies.get(i).setProduct(product);
+            productBodies.get(i).setSequence(i);
+        }
+        product.setProductBodies(productBodies);
+
+        return productBodyMapper.toDto(productBodyRepository.saveAll(productBodies));
+    }
+
 
     public List<ProductBody> updateProductBodies(List<ProductBody> productBodies) {
         if (productBodies.get(0).getId() == null) {
