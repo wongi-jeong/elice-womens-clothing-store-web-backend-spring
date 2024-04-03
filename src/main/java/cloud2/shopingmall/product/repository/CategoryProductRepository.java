8d39fe2b5f9ab1eeaa08badd8ca97d472b6f5e7d
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-04-03 15:54:44 +0900

    feat: findByCategory, findByProduct 추가

--- src/main/java/cloud2/shopingmall/product/repository/CategoryProductRepository.java
+++ src/main/java/cloud2/shopingmall/product/repository/CategoryProductRepository.java
@@ -1,7 +1,15 @@
 package cloud2.shopingmall.product.repository;
 
+import cloud2.shopingmall.product.entity.Category;
 import cloud2.shopingmall.product.entity.CategoryProduct;
+import cloud2.shopingmall.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryProductRepository extends JpaRepository<CategoryProduct,Long> {
+
+    List<CategoryProduct> findByCategory(Category category);
+
+    List<CategoryProduct> findByProduct(Product product);
 }
