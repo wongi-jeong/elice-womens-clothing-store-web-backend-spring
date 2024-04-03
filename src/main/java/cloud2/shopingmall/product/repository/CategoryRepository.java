f2d0ffade24a8e22febf112194d9a0873f7c6991
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-04-03 11:59:50 +0900

    refactor: category_id 값으로 categoryProducts 조회 쿼리 작성

--- src/main/java/cloud2/shopingmall/product/repository/CategoryRepository.java
+++ src/main/java/cloud2/shopingmall/product/repository/CategoryRepository.java
@@ -2,6 +2,10 @@ package cloud2.shopingmall.product.repository;
 
 import cloud2.shopingmall.product.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.categoryProducts WHERE c.id = : id")
+    Category findCategoryWithCategoryProducts(@Param("id") Long id);
 }
