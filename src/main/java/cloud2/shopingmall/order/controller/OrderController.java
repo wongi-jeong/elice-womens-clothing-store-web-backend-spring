efe91efcb308e59bd34457ca6c96d1abf99b86b0
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-04-04 10:36:38 +0900

    수정

--- src/main/java/cloud2/shopingmall/order/controller/OrderController.java
+++ src/main/java/cloud2/shopingmall/order/controller/OrderController.java
@@ -53,30 +53,7 @@ public class OrderController {
         }
         return ResponseEntity.ok(orderDTO);
     }
-<<<<<<< HEAD
-    @PostMapping("/create")
-=======
-    @PostMapping("/creat/cart")
->>>>>>> 297794162e3d170644c2ba94b526f8f5bd90c0b8
-    public ResponseEntity<OrderDTO> createOrderByCart(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-                                                @RequestBody @Validated OrderCreateRequest.OrderByCart orderCreateRequest){
-        //장바구니 주문
-//json으로 여러 객체 받아와야할때 복합 객체를 사용하게 되면 언제 검증을 하게 되는지 그리고 검증에 관한 로직은 어디서 구현하면 좋을 지
-        OrderDTO orderDTO = orderManagementService.createOrderForCart(customUserDetails.getUsername());
-        orderManagementService.createOrderProduct(orderCreateRequest.getOrderProductDTOS(),orderDTO.getId());
-<<<<<<< HEAD
-        return ResponseEntity.ok(orderDTO);
 
-    }
-    @PostMapping("/create")
-=======
-        deliveryService.createDelivery(orderCreateRequest.getDeliveryDTO(),orderDTO.getId());
-        paymentService.createPayment(customUserDetails.getUsername(), orderCreateRequest.getTotalPrice(),orderDTO.getId());
-        return ResponseEntity.ok(orderDTO);
-
-    }
-    @PostMapping("/create/product")
->>>>>>> 297794162e3d170644c2ba94b526f8f5bd90c0b8
     public ResponseEntity<OrderDTO> createOrderByProduct(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                       @RequestBody @Validated OrderCreateRequest.OrderByProduct orderByProduct){
         //상품 주문
