c494515a1f6fb784080cb686c62b4fce4c840677
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-04-12 10:44:15 +0900

    refactor: WebConfig 수정

--- src/main/java/cloud2/shopingmall/ShopingmallApplication.java
+++ src/main/java/cloud2/shopingmall/ShopingmallApplication.java
@@ -14,15 +14,4 @@ public class ShopingmallApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopingmallApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
-
 }
