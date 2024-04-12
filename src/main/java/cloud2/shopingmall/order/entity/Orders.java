c3db5b91985b09bffe7ebbfe4090ded20638e6a5
unknown <yunah9119@gmail.com>
2024-04-12 10:50:29 +0900

    order enum 수정

--- src/main/java/cloud2/shopingmall/order/entity/Orders.java
+++ src/main/java/cloud2/shopingmall/order/entity/Orders.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
 @Setter
 @Getter
@@ -42,29 +43,38 @@ public class Orders extends BaseEntity {
     @JoinColumn(name = "payment_id")
     private Payment payment;
 
+    @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedAt;
+
     public enum OrderStatus {
-        PAYMENT_COMPLETED("결제완료"),
-        PREPARING_FOR_DELIVERY("배송준비"),
-        IN_TRANSIT("배송중"),
-        DELIVERED("배송완료"),
-        ORDER_CANCELLED("주문취소"),
-        REFUND_COMPLETED("환불완료");
+        결제완료(0),
+        배송준비(1),
+        배송중(2),
+        배송완료(3),
+        주문취소(4),
+        환불완료(5);
 
-        private final String description;
+        private final int index;
 
-        OrderStatus(String description){
-            this.description = description;
+        OrderStatus(int index){
+            this.index = index;
         }
-        public String getDescription() {
-            return description;
-        }
-        public static Orders.OrderStatus fromDescription(String description) {
-            for (Orders.OrderStatus status : Orders.OrderStatus.values()) {
-                if (status.getDescription().equals(description)) {
+        public  int getIndex() {
+            return index;
+         }
+
+        public static OrderStatus findByIndex(int index){
+            for (OrderStatus status : OrderStatus.values()) {
+                if (status.index == index) {
                     return status;
                 }
             }
-            throw new IllegalArgumentException("No constant with text " + description + " found");
+            throw new IllegalArgumentException("No constant with index " + index + " found");
         }
     }
     public Orders(Long id,OrderStatus orderStatus){
