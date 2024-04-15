cb4d62f2b95e6322fd96a93539a00a3eff8e3dcd
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-04-15 15:32:29 +0900

    회원정보 수정 페이지

--- src/main/java/cloud2/shopingmall/user/controller/UserController.java
+++ src/main/java/cloud2/shopingmall/user/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -127,9 +128,7 @@ public class UserController {
     // 마이페이지 이동시 현재 로그인한 사용자 정보 조회 기능
     @GetMapping("/myInfor")
     public ResponseEntity<CommonDTO.ShowResponse> showMyInfo(@AuthenticationPrincipal CustomUserDetails userInfo) {
-        if(userInfo == null){
-            throw new NullPointerException("Token is null");
-        }
+
         CommonDTO.ShowResponse dtos = userService.showUser(userInfo);
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
@@ -137,6 +136,7 @@ public class UserController {
     // 사용자 회원정보 변경 기능
     @PatchMapping("/changeInfo")
     public ResponseEntity<String> changeInfo(@AuthenticationPrincipal CustomUserDetails userInfo, @RequestBody CommonDTO.ChangeInfoRequest request) throws PasswordMismatchException {
+
         Boolean result = userService.changeInfo(userInfo, request);
         return ResponseEntity.status(HttpStatus.OK).body("회원정보 변경이 완료되었습니다.");
     }
