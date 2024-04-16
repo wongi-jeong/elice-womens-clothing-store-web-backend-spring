e30a7235e84d22c4b96c9ea82870774bb8cf2445
jogyueun <elice@team01.asia-northeast3-a.c.kdt-cloud-2.internal>
2024-04-16 09:05:53 +0000

    server

--- src/main/java/cloud2/shopingmall/product/controller/ProductController.java
+++ src/main/java/cloud2/shopingmall/product/controller/ProductController.java
@@ -78,8 +78,9 @@ public class ProductController {
                                               @RequestPart("productBodies") List<MultipartFile> productBodiesFile) {
 
 
-        String uploadDir = System.getProperty("user.dir") + "/static/images";
-        String imagePath = "/images/";
+        // String uploadDir = System.getProperty("user.dir") + "/static/images";
+        String uploadDir = "/home/elice/test/static/images";
+        String imagePath = "/static/images/";
 
         // 제목 디렉토리 생성
         File titleDir = new File(uploadDir);
