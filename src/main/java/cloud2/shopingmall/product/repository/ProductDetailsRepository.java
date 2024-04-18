736ade06848b1ce93f87bbaf7e598313ed67b580
unknown <yunah9119@gmail.com>
2024-04-19 01:04:20 +0900

    REFATOR: 상품 주문 후 수량 감소

--- src/main/java/cloud2/shopingmall/product/repository/ProductDetailsRepository.java
+++ src/main/java/cloud2/shopingmall/product/repository/ProductDetailsRepository.java
@@ -7,4 +7,7 @@ import java.util.List;
 
 public interface ProductDetailsRepository extends JpaRepository<ProductDetails,Long> {
     List<ProductDetails> findByProduct_Id(Long productId);
+
+    ProductDetails findByProductIdAndSizeAndColor(Long productId, ProductDetails.Size size, ProductDetails.Color color);
+
 }
